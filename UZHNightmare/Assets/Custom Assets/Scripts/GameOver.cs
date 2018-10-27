664838583668142ff261f3247baf34806d28f617
giik0n <geek4true@gmail.com>
2018-10-27 12:59:10 +0300

    HYM - Improved Physics

---
+++ UZHNightmare/Assets/Custom Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOver : MonoBehaviour {
+GameObject objUI;
+
+void Start () {
+	objUI = GameObject.Find("GameOverText");
+}
+	// Use this for initialization
+	void OnTriggerEnter (Collider plyr) {
+		if(plyr.gameObject.tag == "Player")
+			objUI.GetComponent<Text>().text= "Game Over";
+		
+	}
+}
