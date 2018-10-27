94af92daae9a2e4c7ae24c1860267bb66ca35aac
giik0n <geek4true@gmail.com>
2018-10-27 07:59:17 +0300

    HYM - Just take it off from me

--- UZHNightmare/Assets/Custom Assets/Scripts/ObjectsToCollect.cs
+++ UZHNightmare/Assets/Custom Assets/Scripts/ObjectsToCollect.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class ObjectsToCollect : MonoBehaviour {
+	public static int objects = 0;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		objects++;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter (Collider plyr) {
+		if(plyr.gameObject.tag == "Player")
+			objects--;
+		gameObject.SetActive(false);
 	}
 }
